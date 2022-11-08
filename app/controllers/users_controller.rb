51588afec569f8bf8313e16cfd6c6bbb099bb005
farhan <farhanj.mohamed@gmail.com>
2022-11-08 15:25:19 -0800

    added controller actions and routes to my code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    user = User.all
+    render json: user.as_json
+  end
+
+  def show
+    id = params["id"]
+    user = User.find(id)
+
+    render json: user.as_json
+  end
+
   def create
     user = User.new(first_name: params["first_name"], last_name: params["last_name"], email: params["email"])
     if user.save
@@ -21,5 +33,13 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    id = params["id"]
+    user = User.find(id)
+    user.destroy
+
+    render json: { message: "This has been destroyed" }
   end
 end
