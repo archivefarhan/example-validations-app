9fc72bc999ec851843b048a8f51cc1b20d9d66c7
farhan <farhanj.mohamed@gmail.com>
2022-11-08 17:08:01 -0800

    added a model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true inlusion: {within: @}
+  validates :email, presence: true format: {within: @}
 end
